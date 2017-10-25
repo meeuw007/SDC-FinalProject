2c7e0e6c1c831c1e471faeb355c4aaa1995afc39
Luke Phairatt <luke.phairatt@gmail.com>
2017-10-25 22:00:55 +0200

    StartingMoving

---
+++ ros/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/lukieluke/Documents/Udacity_Courses/SDC/Term3/Lesson25_FinalGroupProject/CarND-Capstone/ros/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/opt/ros/kinetic/lib:/home/lukieluke/.linuxbrew/lib64:/home/lukieluke/.linuxbrew/lib:/usr/local/cuda/lib64:/usr/local/cudnn/lib64"
+export PKG_CONFIG_PATH="/opt/ros/kinetic/lib/pkgconfig:/home/lukieluke/.linuxbrew/lib64/pkgconfig:/home/lukieluke/.linuxbrew/lib/pkgconfig:/usr/local/lib/pkgconfig:/usr/local/lib64/pkgconfig:/usr/lib64/pkgconfig:/usr/lib/pkgconfig:/usr/lib/x86_64-linux-gnu/pkgconfig:/usr/lib64/pkgconfig:/usr/share/pkgconfig"
+export PWD="/home/lukieluke/Documents/Udacity_Courses/SDC/Term3/Lesson25_FinalGroupProject/CarND-Capstone/ros/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/lukieluke/Documents/Udacity_Courses/SDC/Term3/Lesson25_FinalGroupProject/CarND-Capstone/ros/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/lukieluke/Documents/Udacity_Courses/SDC/Term3/Lesson25_FinalGroupProject/CarND-Capstone/ros/src:$ROS_PACKAGE_PATH"
---
+++ ros/src/twist_controller/twist_controller.py
@@ -0,0 +1,15 @@
+
+GAS_DENSITY = 2.858
+ONE_MPH = 0.44704
+
+# mynote: TODO: implement a feedback controller from pid,low pass (both for acceleration), yaw controller (steering)
+
+class Controller(object):
+    def __init__(self, *args, **kwargs):
+        # TODO: Impleme
+        pass
+
+    def control(self, *args, **kwargs):
+        # TODO: Change the arg, kwarg list to suit your needs
+        # Return throttle, brake, steer
+        return 1., 0., 0.
